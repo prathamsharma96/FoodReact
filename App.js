2ed2d6fb42e6a399d04e5fc0e3cec5c989ed77de
Pratham <prathamsharma7648@gmail.com>
2025-11-15 12:05:33 +0530

    laerned to build parcel

--- App.js
+++ App.js
@@ -1,3 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+ 
+ 
+ 
  const heading = React.createElement('h1', {id : "heading"}, "hello world from react");
         const root = ReactDOM.createRoot(document.getElementById("root"));
 
